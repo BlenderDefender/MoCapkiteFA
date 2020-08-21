eed86049bdedcf294879fe4e2d80ccd08cb07115
BlenderDefender <61777131+BlenderDefender@users.noreply.github.com>
2020-08-21 09:11:39 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -226,7 +226,7 @@ class MOCAPKITEFA_APT_Preferences(bpy.types.AddonPreferences):
 		mainrow = layout.row()
 		col = mainrow.column()
 
-        layout.operator("wm.url_open", text="Checkout Gumroad for other addons and more...", icon='FUND').url="https://gumroad.com/blenderdefender"
+        	layout.operator("wm.url_open", text="Checkout Gumroad for other addons and more...", icon='FUND').url="https://gumroad.com/blenderdefender"
 
 
 		# updater draw function
